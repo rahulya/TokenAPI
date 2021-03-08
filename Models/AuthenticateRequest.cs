e1a2c6374ffab83b52caead97e8e531fae2958a6
rahulya <https://github.com/rahulya>
2021-03-08 22:51:48 +0545

    Add Service

--- Models/AuthenticateRequest.cs
+++ Models/AuthenticateRequest.cs
@@ -9,9 +9,9 @@ namespace Token.Models
     public class AuthenticateRequest
     {
         [Required]
-        public string UserName { get; set; }
+        public string Username { get; set; }
 
         [Required]
-        public string Passsword { get; set; }
+        public string Password { get; set; }
     }
 }
